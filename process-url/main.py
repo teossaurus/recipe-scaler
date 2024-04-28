72149c487ef19167ac5117a5537aa67ef7f554a3
Cormick Browne <cormickjbrowne@cormicks-mbp.lan>
2024-04-27 22:28:38 -0400

    process-url mvp

--- process-url/main.py
+++ process-url/main.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import justext
+import html2text
 from io import BytesIO
 
 
@@ -95,7 +96,23 @@ def get_html_with_selenium(url, time_to_sleep=None, tag_to_await=None):
             driver.quit()
     return html_page
 
+# write function that calls a url that's taken as an argument and returns the html
+def get_text_from_url(url):
+    try:
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+        text = html2text.html2text(response.text)
+        print(len(text.split(" ")));
+        return text
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to retrieve HTML: {e}")
+        return ""
+
 @functions_framework.http
 def handler(request):
     data = request.get_json()
-    image = data["image"]
+    url = data["url"]
+    text = get_text_from_url(url)
+    return {
+        "text": text
+    }
