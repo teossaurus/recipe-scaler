635d47441cfd451663e1a2b1d3ba00e6190137f9
Cormick Browne <cormickjbrowne@cormicks-mbp.lan>
2024-04-27 22:44:04 -0400

    Remove unused imports from process-url/main.py

--- process-url/main.py
+++ process-url/main.py
@@ -1,20 +1,11 @@
 # Standard library imports
 import requests
-import time
 from requests.exceptions import RequestException
 
 import functions_framework
 
 # Third party imports
-from selenium import webdriver
-from selenium.common.exceptions import WebDriverException
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-import justext
 import html2text
-from io import BytesIO
 
 # write function that calls a url that's taken as an argument and returns the html
 def get_text_from_url(url):
